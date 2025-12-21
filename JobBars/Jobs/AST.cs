7ab4b608086e8fb777ac1278043fcd8122e93d08
x <x@x.x>
2025-12-21 14:54:00 +0800

    1

--- JobBars/Jobs/AST.cs
+++ JobBars/Jobs/AST.cs
@@ -9,122 +9,173 @@ using JobBars.Gauges.Timer;
 using JobBars.Helper;
 using JobBars.Icons;
 
-namespace JobBars.Jobs {
-    public static class AST {
-        public static GaugeConfig[] Gauges => [
-            new GaugeProcsConfig($"{UiHelper.Localize(JobIds.AST)} {UiHelper.ProcText}", GaugeVisualType.Diamond, new GaugeProcProps {
-                Procs = [
-                    new ProcConfig(UiHelper.Localize(BuffIds.GiantDominance), BuffIds.GiantDominance, ColorConstants.LightBlue)
-                ]
-            }),
-            new GaugeTimerConfig(UiHelper.Localize(BuffIds.Combust3), GaugeVisualType.Bar, new GaugeTimerProps {
-                SubTimers = [
-                    new GaugeSubTimerProps {
+namespace JobBars.Jobs
+{
+    public static class AST
+    {
+        public static GaugeConfig[] Gauges =>
+        [
+            new GaugeProcsConfig( $"{UiHelper.Localize( JobIds.AST )} {UiHelper.ProcText}", GaugeVisualType.Diamond,
+                new GaugeProcProps
+                {
+                    Procs =
+                    [
+                        new ProcConfig( UiHelper.Localize( BuffIds.GiantDominance ), BuffIds.GiantDominance,
+                            ColorConstants.LightBlue )
+                    ]
+                } ),
+            new GaugeTimerConfig( UiHelper.Localize( BuffIds.Combust3 ), GaugeVisualType.Bar, new GaugeTimerProps
+            {
+                SubTimers =
+                [
+                    new GaugeSubTimerProps
+                    {
                         MaxDuration = 30,
                         Color = ColorConstants.LightBlue,
-                        SubName = UiHelper.Localize(BuffIds.Combust3),
-                        Triggers = [
-                            new Item(BuffIds.Combust2),
-                            new Item(BuffIds.Combust3),
-                            new Item(BuffIds.Combust)
+                        SubName = UiHelper.Localize( BuffIds.Combust3 ),
+                        Triggers =
+                        [
+                            new Item( BuffIds.Combust2 ),
+                            new Item( BuffIds.Combust3 ),
+                            new Item( BuffIds.Combust )
                         ]
                     }
                 ]
-            })
+            } )
         ];
 
-        public static BuffConfig[] Buffs => [
-            new BuffConfig(UiHelper.Localize(BuffIds.TheBalance), new BuffProps {
+        public static BuffConfig[] Buffs =>
+        [
+            new BuffConfig( UiHelper.Localize( BuffIds.TheBalance ), new BuffProps
+            {
                 Duration = 15,
                 Icon = ActionIds.TheBalance,
                 Color = ColorConstants.Orange,
-                Triggers = [new Item(BuffIds.TheBalance)],
+                Triggers = [new Item( BuffIds.TheBalance )],
                 ApplyToTarget = true,
                 ShowPartyText = true
-            }),
-            new BuffConfig(UiHelper.Localize(BuffIds.TheSpear), new BuffProps {
+            } ),
+            new BuffConfig( UiHelper.Localize( BuffIds.TheSpear ), new BuffProps
+            {
                 Duration = 15,
                 Icon = ActionIds.TheSpear,
                 Color = ColorConstants.DarkBlue,
-                Triggers = [new Item(BuffIds.TheSpear)],
+                Triggers = [new Item( BuffIds.TheSpear )],
                 ApplyToTarget = true,
                 ShowPartyText = true
-            }),
-            new BuffConfig(UiHelper.Localize(ActionIds.Divination), new BuffProps {
+            } ),
+            new BuffConfig( UiHelper.Localize( ActionIds.Divination ), new BuffProps
+            {
                 CD = 120,
                 Duration = 20,
                 Icon = ActionIds.Divination,
                 Color = ColorConstants.Yellow,
-                Triggers = [new Item(ActionIds.Divination)]
-            })
+                Triggers = [new Item( ActionIds.Divination )]
+            } )
         ];
 
-        public static Cursor Cursors => new( JobIds.AST, CursorType.None, CursorType.CastTime );
+        public static Cursor Cursors => new(JobIds.AST, CursorType.None, CursorType.CastTime);
 
-        public static CooldownConfig[] Cooldowns => [
-            new CooldownConfig(UiHelper.Localize(ActionIds.NeutralSect), new CooldownProps {
+        public static CooldownConfig[] Cooldowns =>
+        [
+            new CooldownConfig( UiHelper.Localize( ActionIds.CollectiveUnconscious ), new CooldownProps
+            {
+                Icon = ActionIds.CollectiveUnconscious,
+                Duration = 10,
+                CD = 60,
+                Triggers = [new Item( ActionIds.CollectiveUnconscious )]
+            } ), new CooldownConfig( UiHelper.Localize( ActionIds.擢升 ), new CooldownProps
+            {
+                Icon = ActionIds.擢升,
+                CD = 60,
+                Triggers = [new Item( ActionIds.擢升 )]
+            } ),
+            new CooldownConfig( UiHelper.Localize( ActionIds.天星冲日 ), new CooldownProps
+            {
+                Icon = ActionIds.天星冲日,
+                CD = 60,
+                Triggers = [new Item( ActionIds.天星冲日 )]
+            } ),
+
+            new CooldownConfig( UiHelper.Localize( ActionIds.NeutralSect ), new CooldownProps
+            {
                 Icon = ActionIds.NeutralSect,
                 Duration = 20,
                 CD = 120,
-                Triggers = [new Item(ActionIds.NeutralSect)]
-            }),
-            new CooldownConfig(UiHelper.Localize(ActionIds.Macrocosmos), new CooldownProps {
+                Triggers = [new Item( ActionIds.NeutralSect )]
+            } ),
+            new CooldownConfig( UiHelper.Localize( ActionIds.Macrocosmos ), new CooldownProps
+            {
                 Icon = ActionIds.Macrocosmos,
                 CD = 180,
                 Duration = 15,
-                Triggers = [new Item(ActionIds.Macrocosmos)]
-            }),
-            new CooldownConfig(UiHelper.Localize(ActionIds.EarthlyStar), new CooldownProps {
+                Triggers = [new Item( ActionIds.Macrocosmos )]
+            } ),
+            new CooldownConfig( UiHelper.Localize( ActionIds.EarthlyStar ), new CooldownProps
+            {
                 Icon = ActionIds.EarthlyStar,
                 Duration = 20,
                 CD = 60,
-                Triggers = [new Item(ActionIds.EarthlyStar)]
-            }),
-            new CooldownConfig($"{UiHelper.Localize(ActionIds.Swiftcast)} ({UiHelper.Localize(JobIds.AST)})", new CooldownProps {
-                Icon = ActionIds.Swiftcast,
-                CD = 40,
-                Triggers = [new Item(ActionIds.Swiftcast)]
-            })
+                Triggers = [new Item( ActionIds.EarthlyStar )]
+            } ),
+            new CooldownConfig( $"{UiHelper.Localize( ActionIds.Swiftcast )} ({UiHelper.Localize( JobIds.AST )})",
+                new CooldownProps
+                {
+                    Icon = ActionIds.Swiftcast,
+                    CD = 40,
+                    Triggers = [new Item( ActionIds.Swiftcast )]
+                } )
         ];
 
-        public static IconReplacer[] Icons => [
-            new IconBuffReplacer(UiHelper.Localize(BuffIds.Combust3), new IconBuffProps {
+        public static IconReplacer[] Icons =>
+        [
+            new IconBuffReplacer( UiHelper.Localize( BuffIds.Combust3 ), new IconBuffProps
+            {
                 IconType = IconActionType.Timer,
-                Icons = [
+                Icons =
+                [
                     ActionIds.Combust1,
                     ActionIds.Combust2,
                     ActionIds.Combust3
                 ],
-                Triggers = [
-                    new IconBuffTriggerStruct { Trigger = new Item(BuffIds.Combust), Duration = 30 },
-                    new IconBuffTriggerStruct { Trigger = new Item(BuffIds.Combust2), Duration = 30 },
-                    new IconBuffTriggerStruct { Trigger = new Item(BuffIds.Combust3), Duration = 30 }
+                Triggers =
+                [
+                    new IconBuffTriggerStruct { Trigger = new Item( BuffIds.Combust ), Duration = 30 },
+                    new IconBuffTriggerStruct { Trigger = new Item( BuffIds.Combust2 ), Duration = 30 },
+                    new IconBuffTriggerStruct { Trigger = new Item( BuffIds.Combust3 ), Duration = 30 }
                 ]
-            }),
-            new IconBuffReplacer(UiHelper.Localize(BuffIds.Divination), new IconBuffProps {
+            } ),
+            new IconBuffReplacer( UiHelper.Localize( BuffIds.Divination ), new IconBuffProps
+            {
                 IconType = IconActionType.Buff,
                 Icons = [ActionIds.Divination],
-                Triggers = [
-                    new IconBuffTriggerStruct { Trigger = new Item(BuffIds.Divination), Duration = 20 }
+                Triggers =
+                [
+                    new IconBuffTriggerStruct { Trigger = new Item( BuffIds.Divination ), Duration = 20 }
                 ]
-            }),
-            new IconBuffReplacer(UiHelper.Localize(BuffIds.Lightspeed), new IconBuffProps {
+            } ),
+            new IconBuffReplacer( UiHelper.Localize( BuffIds.Lightspeed ), new IconBuffProps
+            {
                 IconType = IconActionType.Buff,
                 Icons = [ActionIds.Lightspeed],
-                Triggers = [
-                    new IconBuffTriggerStruct { Trigger = new Item(BuffIds.Lightspeed), Duration = 15 }
+                Triggers =
+                [
+                    new IconBuffTriggerStruct { Trigger = new Item( BuffIds.Lightspeed ), Duration = 15 }
                 ]
-            }),
-            new IconBuffReplacer(UiHelper.Localize(ActionIds.EarthlyStar), new IconBuffProps {
+            } ),
+            new IconBuffReplacer( UiHelper.Localize( ActionIds.EarthlyStar ), new IconBuffProps
+            {
                 IconType = IconActionType.Buff,
-                Icons = [
+                Icons =
+                [
                     ActionIds.EarthlyStar,
                     ActionIds.StellarDetonation
                 ],
-                Triggers = [
-                    new IconBuffTriggerStruct { Trigger = new Item(BuffIds.GiantDominance), Duration = 10 }
+                Triggers =
+                [
+                    new IconBuffTriggerStruct { Trigger = new Item( BuffIds.GiantDominance ), Duration = 10 }
                 ]
-            })
+            } )
         ];
 
         public static bool MP => true;
